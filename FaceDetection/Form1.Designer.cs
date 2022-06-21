e659b607464ebc91e5b155a9deb2cf4c09d7e2a2
Rick Liu <ttt5569y@gmail.com>
2022-06-21 17:18:10 +0800

    搜尋臉部

--- FaceDetection/Form1.Designer.cs
+++ FaceDetection/Form1.Designer.cs
@@ -28,21 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.BtnDetect = new System.Windows.Forms.Button();
+            this.BtnInPut = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(837, 597);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(924, 49);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(166, 165);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // BtnDetect
+            // 
+            this.BtnDetect.Location = new System.Drawing.Point(952, 329);
+            this.BtnDetect.Name = "BtnDetect";
+            this.BtnDetect.Size = new System.Drawing.Size(105, 36);
+            this.BtnDetect.TabIndex = 3;
+            this.BtnDetect.Text = "Detect";
+            this.BtnDetect.UseVisualStyleBackColor = true;
+            this.BtnDetect.Click += new System.EventHandler(this.BtnDetect_Click);
+            // 
+            // BtnInPut
+            // 
+            this.BtnInPut.Location = new System.Drawing.Point(952, 261);
+            this.BtnInPut.Name = "BtnInPut";
+            this.BtnInPut.Size = new System.Drawing.Size(105, 36);
+            this.BtnInPut.TabIndex = 4;
+            this.BtnInPut.Text = "InPut";
+            this.BtnInPut.UseVisualStyleBackColor = true;
+            this.BtnInPut.Click += new System.EventHandler(this.BtnInPut_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(924, 436);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(176, 85);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Detect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1184, 803);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.BtnInPut);
+            this.Controls.Add(this.BtnDetect);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button BtnDetect;
+        private System.Windows.Forms.Button BtnInPut;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- FaceDetection/Form1.cs
+++ FaceDetection/Form1.cs
@@ -10,6 +10,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Emgu.CV;
 using Emgu.CV.Structure;
+using Emgu.CV.Face;
 
 namespace FaceDetection
 {
@@ -23,8 +24,20 @@ namespace FaceDetection
         }
         public class TrainedFaceRecognizer
         {
-            public Emgu.CV.Face.FaceRecognizer faceRecognizer;
-            public TrainedFileList TrainedFilelist;
+            public FaceRecognizer faceRecognizer;
+            public TrainedFileList TrainedFileList;
+        }
+
+        public class TrainedFileListFromImage
+        {
+            public List<Mat> trainedImage = new List<Mat>();
+            public List<int> trainedLabelOrder = new List<int>();
+            public List<string> trainedFileName = new List<string>();
+        }
+        public class TrainedFaceRecognizerFromImage
+        {
+            public FaceRecognizer faceRecognizer;
+            public TrainedFileListFromImage TrainedFileList;
         }
         public class faceDetectedObj
         {
@@ -38,15 +51,19 @@ namespace FaceDetection
             FisherFaceRecognizer = 1,
             LBPHFFaceRecognizer = 2,
         };
-
+        
         public Form1()
         {
             InitializeComponent();
         }
+
+        CascadeClassifier FaceCascadeClassifier;
+        Mat OrgMat = new Mat();
+
         public TrainedFileList SetSampleFacesList()
         {
             TrainedFileList tf = new TrainedFileList();
-            DirectoryInfo di = new DirectoryInfo("hhhh");
+            DirectoryInfo di = new DirectoryInfo("TrainedFace");
             int i = 0;
             foreach (FileInfo fi in di.GetFiles())
             {
@@ -57,10 +74,127 @@ namespace FaceDetection
             }
             return tf;
         }
+        public TrainedFaceRecognizer SetTrainedFaceRecognizer(FaceRecognizerType type)
+        {
+            TrainedFaceRecognizer tfr = new TrainedFaceRecognizer();
+            tfr.TrainedFileList = SetSampleFacesList();
+            switch (type)
+            {
+                case FaceRecognizerType.EigenFaceRecognizer:
+                    tfr.faceRecognizer = new EigenFaceRecognizer(80, double.PositiveInfinity);
+                    break;
+
+                case FaceRecognizerType.FisherFaceRecognizer:
+                    tfr.faceRecognizer = new FisherFaceRecognizer(80, 3500);
+                    break;
+
+                case FaceRecognizerType.LBPHFFaceRecognizer:
+                    tfr.faceRecognizer = new LBPHFaceRecognizer(1, 8, 8, 8, 100);
+                    break; 
+            }
+            tfr.faceRecognizer.Train(tfr.TrainedFileList.trainedImage.ToArray(), tfr.TrainedFileList.trainedLabelOrder.ToArray());
+            return tfr;
+        }
+
+
+        public TrainedFaceRecognizerFromImage SetTrainedFaceRecognizerFromImage(FaceRecognizerType type)
+        {
+
+            TrainedFaceRecognizerFromImage tfr = new TrainedFaceRecognizerFromImage();
+            TrainedFileListFromImage tfList = new TrainedFileListFromImage();
+            tfList.trainedFileName.Add("NA");
+            tfList.trainedImage.Add(new Mat());
+            tfList.trainedLabelOrder.Add(1);
+            tfr.TrainedFileList = tfList;
+            switch (type)
+            {
+                case FaceRecognizerType.EigenFaceRecognizer:
+                    tfr.faceRecognizer = new EigenFaceRecognizer(80, double.PositiveInfinity);
+                    break;
+
+                case FaceRecognizerType.FisherFaceRecognizer:
+                    tfr.faceRecognizer = new FisherFaceRecognizer(80, 3500);
+                    break;
+
+                case FaceRecognizerType.LBPHFFaceRecognizer:
+                    tfr.faceRecognizer = new LBPHFaceRecognizer(1, 8, 8, 8, 100);
+                    break;
+            }
+            Mat[] ff = new Mat[] {new Mat(),new Mat() };
+            Image<Gray, byte> pp = new Image<Gray, byte>(new Size(5,5));
+            Image<Gray,byte>[] ppp = new Image<Gray, byte>[] { pp, pp };
+            int[] hh = new int[] { 1, 2 };
+            List<Mat> eee = new List<Mat>();
+
+
+
+            //tfr.faceRecognizer.Train(new Mat(), hh);
+            return tfr;
+        }
 
 
 
 
+
+        public faceDetectedObj GetFaceRectangle(Mat emguImage)
+        {
+            faceDetectedObj fdo = new faceDetectedObj();
+            fdo.originalImg = emguImage;
+            List<Rectangle> face = new List<Rectangle>();
+            
+            try
+            {
+                Mat ugray = new Mat();
+                CvInvoke.CvtColor(emguImage, ugray, Emgu.CV.CvEnum.ColorConversion.Bgr2Gray);//轉灰階
+                CvInvoke.EqualizeHist(ugray,ugray);// 均衡灰度
+                Rectangle[] faceDetected = FaceCascadeClassifier.DetectMultiScale(ugray, 1.1, 10, new Size(20, 20));
+                face.AddRange(faceDetected);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            fdo.FaceRectangles = face;
+            return fdo;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FaceCascadeClassifier = new CascadeClassifier("haarcascade_frontalface_default.xml");
+        }
+
+        private void BtnInPut_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog path = new OpenFileDialog();
+                if (path.ShowDialog() != DialogResult.OK)
+                    return;
+                OrgMat = CvInvoke.Imread(path.FileName, Emgu.CV.CvEnum.LoadImageType.AnyColor);
+                pictureBox1.Image = OrgMat.Bitmap;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            GC.Collect();
+        }
+
+        private void BtnDetect_Click(object sender, EventArgs e)
+        {
+            faceDetectedObj faceDetectedObj = GetFaceRectangle(OrgMat);
+            Mat drawMat = OrgMat.Clone();
+            foreach (Rectangle rectangle in faceDetectedObj.FaceRectangles)
+            {
+                CvInvoke.Rectangle(drawMat, rectangle,new MCvScalar(0,0,255),2);
+            }
+            pictureBox1.Image = drawMat.Bitmap;
+            GC.Collect();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           TrainedFaceRecognizer ggg = SetTrainedFaceRecognizer(FaceRecognizerType.EigenFaceRecognizer);
+        }
     }
     
 
